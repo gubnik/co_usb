438e5d71ba6ab9395dbe877188a730023731afe7
gubnik <danger8217.nik@gmail.com>
2026-05-12 18:47:40 +0300

    Fix CI

--- docs.sh
+++ docs.sh
@@ -5,5 +5,5 @@ PRESET=no-vcpkg
 cmake --preset $PRESET -DCOUSB_ONLY_BUILD_DOCS=ON && \
 cmake --build build/$PRESET --target docs && \
 mkdir -p docs/html && \
-cp $ROOT/build/$PRESET/docs/html $ROOT/docs/html -r && \
+cp $ROOT/build/$PRESET/docs/html/* $ROOT/docs/html -r && \
 printf "\033[1;33mFinished building docs\033[0;0m\n"
