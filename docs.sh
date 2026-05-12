b656162b18ce8d9d16c455ab6599b93631705000
gubnik <danger8217.nik@gmail.com>
2026-05-12 18:36:19 +0300

    Update CI

--- docs.sh
+++ docs.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/sh
 
 ROOT=$(pwd)
-cmake --preset x64-static -DCOUSB_ONLY_BUILD_DOCS=ON && \
-cmake --build build/x64-static --target docs && \
+PRESET=no-vcpkg
+cmake --preset $PRESET -DCOUSB_ONLY_BUILD_DOCS=ON && \
+cmake --build build/$PRESET --target docs && \
 mkdir -p docs/html && \
-cp $ROOT/build/x64-static/docs/html $ROOT/docs/html -r && \
+cp $ROOT/build/$PRESET/docs/html $ROOT/docs/html -r && \
 printf "\033[1;33mFinished building docs\033[0;0m\n"
